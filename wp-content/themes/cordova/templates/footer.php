3b85326aa72b3608f2bcd929cf44adddcb1a8438
Ed Atrero <eatrero@gmail.com>
2014-11-27 13:05:53 -0800

    fix IG for mobile browser

--- wp-content/themes/cordova/templates/footer.php
+++ wp-content/themes/cordova/templates/footer.php
@@ -5,7 +5,7 @@
     	<div class="row">
 	    	<div class="col-sm-6 col-md-4 col-xs-12">
 		    	<h3 class="h3-span"> Instagram	</h3>
-		    	<div class="col-sm-10 col-md-10 col-lg-10 col-sm-offset-1">
+		    	<div class="col-xs-12 col-sm-10 col-md-10 col-lg-10 col-sm-offset-1">
   		    	<p class='result'></p>
 		    	</div>
 	    	</div>
