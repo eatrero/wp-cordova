f0f50b352303aca278824c09732abc43baccec23
Ed Atrero <eatrero@gmail.com>
2014-11-27 10:52:25 -0800

    fix pagination for orphan posts

--- wp-content/themes/cordova/index.php
+++ wp-content/themes/cordova/index.php
@@ -30,11 +30,13 @@
   <?php endwhile; ?>
 
 <?php if ($wp_query->max_num_pages > 1) : ?>
-  <nav class="post-nav">
-    <ul class="pager">
-      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
-      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
-    </ul>
-  </nav>
+  <div class="row">
+    <nav class="post-nav">
+      <ul class="pager">
+        <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+        <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+      </ul>
+    </nav>
+  </div>
 <?php endif; ?>
 </div>
