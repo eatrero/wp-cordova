6a0aa849d49f5385e841b9e22d869fa7f64b34d4
Ed Atrero <eatrero@gmail.com>
2015-02-25 22:43:05 -0800

    added blog share buttons

--- wp-content/themes/cordova/templates/comments.php
+++ wp-content/themes/cordova/templates/comments.php
@@ -78,9 +78,13 @@
     <div class='col-sm-4 col-sm-offset-1'>
         <h3>Share this post</h3>
         <?php $perm = get_permalink();?>
-        <a href="https://www.facebook.com/dialog/share?app_id=814169431964357&display=popup&href=<?php echo $perm;?>&redirect_uri=<?php echo $perm;?>">facebook</a>
-
-        <div class="fb-share-button" data-href="<?php echo $perm;?>" data-layout="link">facebook</div>
+        <a href="https://www.facebook.com/dialog/share?app_id=814169431964357&display=popup&href=<?php echo $perm;?>&redirect_uri=<?php echo $perm;?>" target="_blank"><i class="fa fa-facebook-official fa-3x"></i></a>
+        <a class="twitter-share-button"
+           href="https://twitter.com/share?text=<?php echo urlencode(get_the_title()); ?>"
+          data-url="<?php echo $perm;?>"
+          data-count="vertical" target="_blank">
+        <i class="fa fa-twitter-square fa-3x"></i>
+        </a>
     </div>
   <?php endif; ?>
 </section><!-- /#respond -->
