174c4acbdd27e0905745cc57eed51183d686ab81
Ed Atrero <eatrero@gmail.com>
2015-03-30 20:46:54 -0700

    add social sharing for locations

---
+++ wp-content/themes/cordova/template-location.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Location Template
+*/
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+<h1 class="post-title"><?php echo get_the_title(); ?></h1>
+
+  <?php get_template_part('templates/page', 'header'); ?>
+<div style='text-align:center;margin-top:10px;line-height:20px;'>
+    <p>Share this post</p>
+    <?php $perm = get_permalink();?>
+    <a href="https://www.facebook.com/dialog/share?app_id=814169431964357&display=popup&href=<?php echo $perm;?>&redirect_uri=<?php echo $perm;?>" target="_blank"><i class="fa fa-facebook-official fa-2x"></i></a>
+    <a class="twitter-share-button"
+       href="https://twitter.com/share?text=<?php echo urlencode(get_the_title()); ?>"
+      data-url="<?php echo $perm;?>"
+      data-count="vertical" target="_blank">
+    <i class="fa fa-twitter-square fa-2x"></i>
+    </a>
+</div>
+<hr class='hr-after-meta'>
+
+  <?php get_template_part('templates/content', 'page'); ?>
+
+<?php endwhile; ?>
+
+<div style='text-align:center;margin-top:10px;line-height:20px;margin-bottom:50px;'>
+    <h3>Share this post</h3>
+    <?php $perm = get_permalink();?>
+    <a href="https://www.facebook.com/dialog/share?app_id=814169431964357&display=popup&href=<?php echo $perm;?>&redirect_uri=<?php echo $perm;?>" target="_blank"><i class="fa fa-facebook-official fa-3x"></i></a>
+    <a class="twitter-share-button"
+       href="https://twitter.com/share?text=<?php echo urlencode(get_the_title()); ?>"
+      data-url="<?php echo $perm;?>"
+      data-count="vertical" target="_blank">
+    <i class="fa fa-twitter-square fa-3x"></i>
+    </a>
+</div>
+</div>
--- wp-content/themes/cordova/templates/entry-meta.php
+++ wp-content/themes/cordova/templates/entry-meta.php
@@ -2,4 +2,15 @@
 <time class="published post-date" datetime="<?php echo get_the_time('c'); ?>">
 	<?php echo get_the_date(); ?>
 </time>
+<div style='text-align:center;margin-top:10px;line-height:20px;'>
+    <p>Share this post</p>
+    <?php $perm = get_permalink();?>
+    <a href="https://www.facebook.com/dialog/share?app_id=814169431964357&display=popup&href=<?php echo $perm;?>&redirect_uri=<?php echo $perm;?>" target="_blank"><i class="fa fa-facebook-official fa-2x"></i></a>
+    <a class="twitter-share-button"
+       href="https://twitter.com/share?text=<?php echo urlencode(get_the_title()); ?>"
+      data-url="<?php echo $perm;?>"
+      data-count="vertical" target="_blank">
+    <i class="fa fa-twitter-square fa-2x"></i>
+    </a>
+</div>
 <hr class='hr-after-meta'>
