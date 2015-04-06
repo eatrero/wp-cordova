01f2911322da8645763f0438b37b7a423130966e
Ed Atrero <eatrero@gmail.com>
2015-04-06 07:03:56 -0700

    fix facebook link

--- wp-content/themes/cordova/template-location.php
+++ wp-content/themes/cordova/template-location.php
@@ -11,7 +11,7 @@ Template Name: Location Template
 <div style='text-align:center;margin-top:10px;line-height:20px;'>
     <p>Share this post</p>
     <?php $perm = get_permalink();?>
-    <a href="https://www.facebook.com/dialog/share?app_id=814169431964357&display=popup&href=<?php echo $perm;?>&redirect_uri=<?php echo $perm;?>" target="_blank"><i class="fa fa-facebook-official fa-2x"></i></a>
+    <a href="https://www.facebook.com/sharer.php?u=<?php echo urlencode(get_permalink($post->ID)); ?>&t=<?php echo urlencode($post->post_title); ?>"><i class="fa fa-facebook-official fa-2x"></i></a>
     <a class="twitter-share-button"
        href="https://twitter.com/share?text=<?php echo urlencode(get_the_title()); ?>"
       data-url="<?php echo $perm;?>"
